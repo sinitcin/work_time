fd167b0ea1c4afe4f7eed8e75a63e56b471aedaa
anton <anton@debian>
2017-06-09 18:00:13 +0300

    Используем try! вместо wrap

--- src/data_base/mod.rs
+++ src/data_base/mod.rs
@@ -4,36 +4,54 @@ extern crate sqlite;
 use std::fs;
 use std::path::Path;
 use std::result::Result;
-
-use std::num::ParseIntError;
+use std::io::Error;
 
 /// Ошибка SQLiteError - позволяет получить код события повлекшего к ошибке
-pub struct SQLiteError {
-    code: i32,
+#[derive(Debug)]
+pub enum SQLiteError {
+    CantRemoveFile,
+    CantOpenConnect,
 }
 
 // Объявляем основной альяс для `Result` для типа ошибки `SQLiteResult`.
-type SQLiteResult<T> = Result<T, SQLiteError>;
+pub type SQLiteResult<T> = Result<T, SQLiteError>;
+
+impl From<Error> for SQLiteError {
+    #[allow(dead_code)]
+    #[allow(unused_variables)]
+    fn from(other: Error) -> Self {
+        SQLiteError::CantRemoveFile
+    }
+}
+
+impl From<sqlite::Error> for SQLiteError {
+    #[allow(dead_code)]
+    #[allow(unused_variables)]
+    fn from(other: sqlite::Error) -> Self {
+        SQLiteError::CantOpenConnect
+    }
+}
+
 
 /// Создаёт базу данных sqlite по указанному пути.
 ///
 /// # Ошибки
 ///
-/// Эта функция должна вернуть ошибку которая расскажет Вам о ситуации 
+/// Эта функция должна вернуть ошибку которая расскажет Вам о ситуации
 /// повлекшей к этой ошибке. Есть несколько основных причин:
 ///
 /// * Путь задан к каталогу, а не к файлу
 /// * Файл занят другой программой
 /// * Нет соответствующих библиотек
 ///
-/// # Пример использования 
+/// # Пример использования
 ///
 /// ```rust
 /// # fn foo() -> std::io::Result<()> {
 /// mod data_base;
 ///
 /// let res = data_base::create("/some/file/path.sqlite").unwrap();
-/// 
+///
 /// # }
 /// ```
 pub fn create(file_path: &str) -> SQLiteResult<()> {
@@ -41,15 +59,11 @@ pub fn create(file_path: &str) -> SQLiteResult<()> {
     // Если БД есть, то удаляем её
     if Path::new(file_path).exists() {
 
-        let result = fs::remove_file(file_path);
-        match result {
-            Some(_) => {/* Успех */},
-            None => return Err(1),
-        }
+        try!(fs::remove_file(file_path));
     }
 
     // Создаём соединение и бд автокрейтиться
-    let connection = sqlite::open(file_path).unwrap();
+    let connection = try!(sqlite::open(file_path));
 
     let sql_commands = "\
     CREATE TABLE users (name TEXT, age INTEGER);\
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod data_base;
 
 fn main() {
     println!("Hello, world!");
+    #[allow(unused_variables)]
     let code = data_base::create("test.sqlite");
 }
